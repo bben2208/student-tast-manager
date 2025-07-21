0c1df4b047682986f6c4ecd5983678c068621b09
Benjamin Bognar <benjaminbognar@MacBook-Pro-2.local>
2025-07-21 20:00:50 +0100

    debug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ required so Vite builds correct relative paths
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050', // for local dev only
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace(/^\/api/, ''),
